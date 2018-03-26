eec2307341f960a98cbd3b9277d44f8f49630515
Dzmtiry Hardziayuk <redaxo@gmail.com>
2018-03-26 17:09:44 +0300

    added autoconfigure, decomposition for docker stage

--- vars/javaCoursesPipeline.groovy
+++ vars/javaCoursesPipeline.groovy
@@ -4,22 +4,10 @@ def call(body) {
     body.delegate = config
     body()
 
-    if (config.modules == null) {
-        config.modules = [[]]
-    }
+    def dockerRegistry = "127.0.0.1:80"
+    def username = scm.getUserRemoteConfigs()[0].getUrl().tokenize('/')[3].split("\\.")[0].toLowerCase() ?: "undefined_user"
 
-    config.username = scm.getUserRemoteConfigs()[0].getUrl().tokenize('/')[3].split("\\.")[0].toLowerCase()
-    println scm
-    println scm.getUserRemoteConfigs()
-    println scm.getUserRemoteConfigs()[0].getUrl()
-    println scm.getUserRemoteConfigs()[0].getUrl().tokenize('/')[3]
-    println scm.getUserRemoteConfigs()[0].getUrl().tokenize('/')[3].split("\\.")[0]
-    println scm.getUserRemoteConfigs()[0].getUrl().tokenize('/')[3].split("\\.")[0].toLowerCase()
-    if (!config.username) {
-        config.username = "undefined_user"
-    }
-    println config.username
-    final String repositoryUrl = "127.0.0.1:80/java_courses_2018/${config.username}".toLowerCase();
+    final String repositoryUrl = "$dockerRegistry/java_courses_2018/${username}".toLowerCase()
 
     node() {
         stage("Checkout") {
@@ -31,9 +19,12 @@ def call(body) {
             sh "printenv"
         }
 
+        def scanResult
         stage("Install") {
             try {
                 sh "mvn clean install -DskipTests"
+
+                scanResult = scanProject()
             } finally {
                 try {
                     junit '**/target/surefire-reports/*.xml'
@@ -43,46 +34,36 @@ def call(body) {
         }
 
         stage("Docker") {
-            def exposePorts = []// provided ports will be added to Dockerfile, then all  ports could be exposed by '-P' flag in docker run
-            StringBuilder startScript = new StringBuilder("#!/bin/sh\n")
-            sh "mkdir -p deploy_modules"
-            sh "rm -f deploy_modules/*"
-            def dockerfile = """
-FROM jetty:9.4-alpine
-ADD http://central.maven.org/maven2/org/eclipse/jetty/jetty-runner/9.4.8.v20171121/jetty-runner-9.4.8.v20171121.jar /usr/local/jettyapps/
-COPY ./deploy_modules/* /usr/local/jettyapps/"""
 
-            for (def module : config.modules) {
-                def file = findFiles(glob: "${module.name}/target/*.*ar")[0]
-                echo "Copying file: $file"
-                exposePorts.add(module.port)
-                dockerfile+="\nEXPOSE ${module.port}"
-                startScript.append("nohup java -jar /usr/local/jettyapps/jetty-runner-9.4.8.v20171121.jar --port ${module.port} /usr/local/jettyapps/${file.name} > springlog.log 2>&1 &\n")
-                sh "cp $file deploy_modules/"
-            }
-            dockerfile+="""
-COPY jettystart.sh /usr/local/bin/
-USER root
-RUN chmod 755 /usr/local/jettyapps/jetty-runner-9.4.8.v20171121.jar
-USER jetty
-ENTRYPOINT ["jettystart.sh"]"""
+            def startScript = generateStartScript(scanResult)
+            def dockerfile = generateDockerfile(scanResult)
 
-            startScript.append("echo \"Servers started\"\n")
-            startScript.append("tail -f springlog.log")
-            echo "${startScript.toString()}"
-            echo "${dockerfile}"
+            copyFiles(scanResult)
 
             writeFile file: "Dockerfile", text: dockerfile
             writeFile file: "jettystart.sh", text: startScript.toString()
             sh "chmod +x jettystart.sh"
             sh "chmod 755 deploy_modules/*"
 
+
+            println 'Start script'
+            println startScript
+            println 'Start script'
+
+            println 'Dockerfile'
+            println dockerfile
+            println 'Dockerfile'
+
+
             withDockerServer([uri: "unix:///var/run/docker.sock"]) {
                 withDockerRegistry([url: "http://127.0.0.1:80/"]) {
 
                     def buildarg = ""
-                    for (int port : exposePorts)
-                        buildarg += "--build-arg ports=$port "
+
+                    scanResult.each { def module ->
+                        buildarg += "--build-arg ports=$module.port "
+                    }
+
                     def image = docker.build(repositoryUrl, "$buildarg.")
                     image.push()
                 }
@@ -90,7 +71,7 @@ ENTRYPOINT ["jettystart.sh"]"""
         }
 
         stage("Start container") {
-            final String containerName = config.username.toLowerCase()
+            final String containerName = username.toLowerCase()
             sh "docker stop $containerName || exit 0"
             sh "docker rm $containerName || exit 0"
             sh "docker run -dP --name $containerName $repositoryUrl"
@@ -103,3 +84,85 @@ ENTRYPOINT ["jettystart.sh"]"""
         }
     }
 }
+
+
+def scanProject() {
+    def executables = []
+    def pom = readMavenPom file: 'pom.xml'
+    pom.getModules().each { String module ->
+
+        def module_pom = readMavenPom file: "$module/pom.xml"
+
+        module_pom?.build?.plugins?.each { def plugin ->
+
+            if ("jetty-maven-plugin" == plugin.artifactId) {
+                def artifactId = module_pom.artifactId
+                def version = module_pom.version ? module_pom.version : module_pom?.parent?.version
+                def packaging = module_pom.packaging
+                def file = "$artifactId-$version.$packaging"
+                def filePath = "$artifactId/target/"
+                def config = plugin?.configuration
+
+                def path = config?.getChild('webApp')?.getChild('contextPath')?.value
+                def port = config?.getChild('httpConnector')?.getChild('port')?.value
+
+                path = path ?: '/'
+                port = port ?: 8080
+
+                executables.add([
+                        port    : port,
+                        path    : path,
+                        file    : file,
+                        filePath: filePath])
+            }
+        }
+    }
+    executables
+}
+
+def generateStartScript(def modules) {
+
+    StringBuilder startScript = new StringBuilder("#!/bin/sh\n")
+
+    modules.each { def module ->
+        startScript.append("nohup java -jar /usr/local/jettyapps/jetty-runner-9.4.8.v20171121.jar --port ${module.port} /usr/local/jettyapps/${module.file} > springlog.log 2>&1 &\n")
+    }
+
+    startScript.append("echo \"Servers started\"\n")
+    startScript.append("tail -f springlog.log")
+
+    startScript
+}
+
+def generateDockerfile(def modules) {
+
+    def dockerfile = """
+FROM jetty:9.4-alpine
+ADD http://central.maven.org/maven2/org/eclipse/jetty/jetty-runner/9.4.8.v20171121/jetty-runner-9.4.8.v20171121.jar /usr/local/jettyapps/
+COPY ./deploy_modules/* /usr/local/jettyapps/"""
+
+    modules.each { def module ->
+        dockerfile += "\nEXPOSE ${module.port}"
+    }
+
+    dockerfile += """
+COPY jettystart.sh /usr/local/bin/
+USER root
+RUN chmod 755 /usr/local/jettyapps/jetty-runner-9.4.8.v20171121.jar
+USER jetty
+ENTRYPOINT ["jettystart.sh"]"""
+
+
+    dockerfile
+}
+
+def copyFiles(def modules) {
+    sh "mkdir -p deploy_modules"
+    sh "rm -f deploy_modules/*"
+
+    modules.each { def module ->
+        def file = module.filePath + module.file
+        sh "cp $file deploy_modules/"
+    }
+}
+
